0056157c180892ec4e58c04c7d03faf91c5e0e91
amirramzi <146312907+amirramzi@users.noreply.github.com>
2024-09-06 15:15:35 +0330

    create dates and location section

---
+++ src/app/components/sections/location/Location.tsx
@@ -0,0 +1,60 @@
+"use client";
+import React, { useState } from "react";
+import styles from "./Location.module.scss";
+import Calendar from "../../icon/Calendar";
+import AustriaMap from "../../icon/AustriaMap";
+
+const Location: React.FC = () => {
+  const [activeCard, setActiveCard] = useState<number | null>(null);
+
+  const clickHandler = (index: number) => {
+    setActiveCard(index);
+  };
+
+  const LocCard: { name: string }[] = [
+    { name: "Stadthalle Steyr" },
+    { name: "Sporthalle Viktring Klagenfurt" },
+    { name: "Sporthalle Villach" },
+    { name: "Messezentrum Dornbirn" },
+    { name: "Sporthalle Baden" },
+    { name: "Sporthalle Eferding" },
+    { name: "Sporthalle Leonding" },
+    { name: "Fischamend" },
+    { name: "Sporthalle Rum" },
+    { name: "Augustinum Halle Graz" },
+    { name: "Sporthalle Hausmannstätten" },
+    { name: "Sporthalle Baden" },
+    { name: "Stadium No. 13" },
+    { name: "Stadium No. 14" },
+    { name: "Stadium No. 15" },
+    { name: "Stadium No. 16" },
+  ];
+
+  return (
+    <div className={styles.wrapper}>
+      <h3>Dates & Locations</h3>
+      <div className={styles.cardWrapper}>
+        {LocCard.map((item, index) => (
+          <div
+            key={index}
+            onClick={() => clickHandler(index)}
+            className={
+              activeCard === index
+                ? `${styles.locCard} ${styles.active}`
+                : styles.locCard
+            }
+          >
+            <h6>{item.name}</h6>
+            <div className={styles.date}>
+              <Calendar />
+              <span>Sat, 25 Nov 2023</span>
+            </div>
+          </div>
+        ))}
+      </div>
+      <AustriaMap className={styles.map} />
+    </div>
+  );
+};
+
+export default Location;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import About from "./components/sections/about/About";
 import Hero from "./components/sections/hero/Hero";
+import Location from "./components/sections/location/Location";
 import Place from "./components/sections/place/Place";
 
 import styles from "./page.module.scss";
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Place />
       <About />
+      <Location />
     </>
   );
 }
